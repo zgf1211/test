bc93a0d47b973761f4dc5bdeb00269c363045c1c
zgf <zgf@192.168.56.1>
2020-08-13 14:49:16 +0800

    初始化

---
+++ src/main/java/com/test.java
@@ -0,0 +1,17 @@
+package com;/**
+ * @ClassName test
+ * @Description: TODO
+ * @Author Administrator
+ * @Date 2020/8/13
+ * @Version V1.0
+ **/
+
+/**
+ * @ClassName test
+ * @Description: TODO
+ * @Author Administrator
+ * @Date 2020/8/13 
+ * @Version V1.0
+ **/
+public class test {
+}
