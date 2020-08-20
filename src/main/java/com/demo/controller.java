b2d7460ebbbc7d1d8528bc6b78f4e8e7c154271d
zgf <zgf@192.168.56.1>
2020-08-20 10:45:01 +0800

    初始化

---
+++ src/main/java/com/demo/controller.java
@@ -0,0 +1,26 @@
+package com.demo;/**
+ * @ClassName controler
+ * @Description: TODO
+ * @Author Administrator
+ * @Date 2020/8/13
+ * @Version V1.0
+ **/
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName controler
+ * @Description: TODO
+ * @Author Administrator
+ * @Date 2020/8/13
+ * @Version V1.0
+ **/
+@RestController
+@RequestMapping("/test")
+public class controller {
+    @RequestMapping("/1")
+    public String getString() {
+        return  "success";
+    }
+}
